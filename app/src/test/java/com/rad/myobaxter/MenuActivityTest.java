add197d3b78600300b2ee47954de4d91c131ad71
Andrew Robinson <andrew.robinson@kotikan.com>
2015-04-13 23:45:30 +0100

    Update robolectric to 3.0rc2 and removed custom test runner that is now unnecessary

--- app/src/test/java/com/rad/myobaxter/MenuActivityTest.java
+++ app/src/test/java/com/rad/myobaxter/MenuActivityTest.java
@@ -6,27 +6,31 @@ import android.widget.Button;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.robolectric.Robolectric.shadowOf;
+import static org.robolectric.Robolectric.*;
+import static org.robolectric.Shadows.shadowOf;
+import static org.robolectric.shadows.ShadowView.clickOn;
 
 
 @RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
 public class MenuActivityTest {
 
     private MenuActivity menuActivity;
 
     @Before
     public void setUp() throws Exception {
-        menuActivity = Robolectric.setupActivity(MenuActivity.class);
+        menuActivity = setupActivity(MenuActivity.class);
     }
 
     @Test
     public void dataLogButtonTapDisplaysDataLogActivity(){
         Button button = (Button) menuActivity.findViewById(R.id.data_log_button);
         assertThat(button.getText()).isEqualTo(menuActivity.getString(R.string.data_log));
-        Robolectric.clickOn(button);
+        clickOn(button);
         Intent expectedIntent = new Intent(menuActivity, DataLogActivity.class);
         assertThat(shadowOf(menuActivity).getNextStartedActivity()).isEqualTo(expectedIntent);
     }
@@ -35,7 +39,7 @@ public class MenuActivityTest {
     public void helloWorldButtonTapDisplaysHelloWorldActivity(){
         Button button = (Button) menuActivity.findViewById(R.id.hello_world_button);
         assertThat(button.getText()).isEqualTo(menuActivity.getString(R.string.hello_world));
-        Robolectric.clickOn(button);
+        clickOn(button);
         Intent expectedIntent = new Intent(menuActivity, HelloWorldActivity.class);
         assertThat(shadowOf(menuActivity).getNextStartedActivity()).isEqualTo(expectedIntent);
     }
